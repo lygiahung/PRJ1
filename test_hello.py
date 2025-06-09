282d3df09debaf3765807a2efa8daa40b19155e4
lygiahung <lygiahung2612@gmail.com>
2025-06-09 11:14:13 +0700

    cc

--- test_hello.py
+++ test_hello.py
@@ -3,7 +3,7 @@ from hello import greet
 
 class TestGreet(unittest.TestCase):
     def test_greet(self):
-        self.assertEqual(greet("World"), "Hello, World!")
+        self.assertEqual(greet("lygiahung"), "Hello, World!")
 
 if __name__ == "__main__":
     unittest.main()
